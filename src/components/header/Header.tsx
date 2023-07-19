b95b87d7eb5b4970b4b33f22b444283380e37cbd
luisfall <luisfallas31@gmail.com>
2023-07-18 20:21:29 -0600

    [ADD] Fixes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,25 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import Link from 'next/link'
 import React, { useRef } from 'react'
 
 const Header = () => {
    return (
-      <div className="row h-100 w-110 bg-primary justify-content-center align-content-center">
+      <div className="row w-100 bg-primary justify-content-center align-content-center">
          <div className="col-12 col-sm-4 col-md-4 col-lg-3 col-xl-3 text-white d-flex justify-content-center align-items-center">
             <h1 className="m-2 font-monospace">Portfolio</h1>
          </div>
          <div className="col-12 col-sm-9 col-md-8 col-lg-9 col-xl-9 d-flex justify-content-center align-items-center">
-            <a className="btn flex-fill btn-secondary m-1 h-75 rounded-5 shadow border text-md" href="#personalInfo">
+            <Link className="btn flex-fill  m-1 h-75 rounded-5 shadow border text-md" href="#personalInfo">
                Personal
-            </a>
-            <a className="btn flex-fill btn-secondary m-1 h-75 rounded-5 shadow border" href="#carousel">
+            </Link>
+            <Link className="btn flex-fill m-1 h-75 rounded-5 shadow border" href="#carousel">
                Projects
-            </a>
-            <a className="btn flex-fill btn-secondary m-1 h-75 rounded-5 shadow border" href="#callToAction">
+            </Link>
+            <Link className="btn flex-fill m-1 h-75 rounded-5 shadow border" href="#callToAction">
                Call To Action
-            </a>
-            <a className="btn flex-fill btn-secondary m-1 h-75 rounded-5 shadow border" href="#footer">
+            </Link>
+            <Link className="btn flex-fill m-1 h-75 rounded-5 shadow border" href="#footer">
                Footer
-            </a>
+            </Link>
          </div>
       </div>
    )
